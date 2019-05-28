2e0b7ff53cd051e983908a3d4a2c75c9d9c75338
issaclin32 <issaclin32@gmail.com>
2019-05-28 18:31:35 +0800

    restructuring

--- dict_scrape/__init__.py
+++ dict_scrape/__init__.py
@@ -1,6 +1,5 @@
-import scripts
-from scripts.cam import Cam
-from scripts.mw import MW_Dictionary, MW_Thesaurus
+from .scripts.cam import Cam
+from .scripts.mw import MW_Dictionary, MW_Thesaurus
 from .data_types import OnlineDict, Lang
 
 # TODO: unified dict result class that applies to all kinds of dictionary results
